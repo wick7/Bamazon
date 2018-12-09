89b8fa59746df7a08af16788a8be7b9d7f95ed3a
wick7 <wickersham.craig@gmail.com>
2018-12-09 00:30:21 -0800

    update

---
+++ CustomerView/bamazon.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  item_id INT NULL,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(45) NULL,
+  price DECIMAL(10, 4) NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3020, "Monitor", "Electronics", 149.99, 15), 
+        (3021, "1 TB Storage Drive", "Electronics", 99.99, 40),
+        (3022, "Shirt", "Clothing", 19.99, 60),
+        (3023, "Pants", "Clothing", 29.99, 20), 
+        (3024, "Shrek 2", "Entertainment", 14.99, 20),
+        (3025, "The Place Beyond The Pines", "Entertainment", 19.99, 10),
+        (3026, "Apple", "Food", 0.99, 6), 
+        (3027, "Pie", "Food", 8.99, 12), 
+        (3028, "Cold Medicine", "Health", 9.99, 20), 
+        (3029, "Asprin", "Health", 7.99, 20); 
+        
+
+-- ### Alternative way to insert more than one row
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+
+-- item_id (unique id for each product)
+-- product_name (Name of product)
+-- department_name
+-- price (cost to customer)
+-- stock_quantity (how much of the product is available in stores)
---
+++ CustomerView/bamazonCustomer.js
@@ -0,0 +1,103 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "clarks77",
+  database: "bamazon"
+});
+
+
+  function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+    //   console.log(res);
+    var currentChoices = []
+      for(var i in res) { 
+          var final = `
+            ${res[i].product_name} | $${res[i].price} | ID: ${res[i].item_id}
+          `
+          console.log(final)
+          currentChoices.push(res[i].product_name)
+      }
+      
+        inquirer.prompt([
+            // {
+            //     type: "input",
+            //     name: "item_id",
+            //     message: "Enter the Product ID of the Item you would like to purchase: "
+            // },
+            {
+                type: "list",
+                name: "product",
+                message: "Please select you item: ",
+                choices: currentChoices
+            }, 
+            {
+                type: "input",
+                name: "units",
+                message: "Enter number of units you would like: "
+            }, 
+        ]).then(function(answers) {
+            // console.log(answers.product)
+            // console.log(answers.units)
+            connection.query(`SELECT stock_quantity FROM products WHERE product_name = "${answers.product}"`, function(err, res) {
+                if (err) throw err;
+                console.log(res[0].stock_quantity);
+                if(res[0].stock_quantity < answers.units) {
+                    console.log("Insufficient quantity left! Please choose less.");
+                    readProducts();
+                }else if(res[0].stock_quantity >= answers.units) {
+                    var newStock = res[0].stock_quantity - answers.units;
+                    console.log("Updating all " + answers.product + " quantities...\n");
+                    console.log("Previous " + answers.product + " stock " + res[0].stock_quantity);
+                    console.log("---------------------------------------")
+                    console.log("New " + answers.product + " stock " + newStock);
+                    updateProducts(newStock, answers.product, answers.units)
+                }
+              });
+        });
+    });
+  }
+
+readProducts()
+
+// select * from products
+
+function updateProducts(stockUpdate, product, quantity) {
+    
+    var query = connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+            stock_quantity: stockUpdate
+        },
+        {
+            product_name: product
+        }
+      ],
+      function(err, res) {
+        console.log("\n" + res.affectedRows + " stock update!\n");
+      }
+    );
+    // console.log(query.sql);
+    connection.query(`SELECT price FROM products WHERE product_name = "${product}"`, function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(`Total Due = $${quantity * res[0].price} \n`);
+        connection.end();
+      });
+  }
+
+
+
